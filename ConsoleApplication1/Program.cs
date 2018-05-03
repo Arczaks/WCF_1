a5a6d4fb7c80583172be71ddd6e8d191a7329488
Arczaks <archaxwertq@gmail.com>
2018-05-03 16:19:31 +0200

    klient

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Discovery;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //z IsCompleted
+            var client = new ServiceReference1.EngineClient();
+            IAsyncResult res = client.Beginstart(null, null);
+            while (!res.IsCompleted) ;
+            string wynik = client.Endstart(res);
+            Console.WriteLine("1");
+            Console.WriteLine(wynik);
+            client.Close();
+
+            //z AsyncWaitHandle.WaitOne
+            var client2 = new ServiceReference1.EngineClient();
+            IAsyncResult res2 = client2.Beginstart(null, null);
+            res2.AsyncWaitHandle.WaitOne();
+            string wynik2 = client2.Endstart(res2);
+            Console.WriteLine("2");
+            Console.WriteLine(wynik2);
+            client2.Close();
+        }
+    }
+}
